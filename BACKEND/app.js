d07c3ef04d5d5afcd1967e104cb50b107f24f61c
Simran Singh <simransingh25060@gmail.com>
2025-12-17 02:58:21 +0530

    Project initialized, mongoDB connected

---
+++ BACKEND/app.js
@@ -0,0 +1,42 @@
+import 'dotenv/config'; 
+import express from 'express';
+import {nanoid} from  "nanoid"
+import connectDB from "./src/config/mongo.config.js"
+import urlSchema from "./src/models/shorturl.model.js"
+
+
+
+const app = express();
+
+app.use(express.json()) //body parser
+app.use(express.urlencoded({extended:true})) 
+
+app.post("/api/create", (req,res)=> {
+    const {url} = req.body
+    const shortUrl = nanoid(7)
+    const newUrl = new urlSchema({
+        full_url: url,
+        short_url: shortUrl
+    })
+    newUrl.save()
+    res.send(nanoid(7))
+})
+
+app.get("/:id", async (req,res) => {
+    const {id} = req.params
+    const url = await urlSchema.findOne({short_url: id})
+    if (url) {
+        res.redirect(url.full_url)
+    }   else {
+    res.status(404).json("Not found")
+    }
+})
+
+app.listen(3001, () => {
+    connectDB()
+    console.log("Server is running on port http://localhost:3001")
+})
+
+//GET- redirection
+//POST- create short url
+//nanoid - generate unique IDs, no default export
