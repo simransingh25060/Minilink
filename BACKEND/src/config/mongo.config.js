c382fa670b497a290b88ba07e6af64ec646c1f80
Simran Singh <simransingh25060@gmail.com>
2026-01-31 23:07:29 +0530

    Update mongo.config.js

--- BACKEND/src/config/mongo.config.js
+++ BACKEND/src/config/mongo.config.js
@@ -1,14 +1,39 @@
 import mongoose from "mongoose";
-console.log(process.env.MONGO_URI);
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+    
+    if (!process.env.MONGO_URI) {
+        throw new Error("MONGO_URI is not defined in environment variables");
     }
+
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+      return mongoose;
+    });
+  }
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e) {
+    cached.promise = null;
+    throw e;
+  }
+  
+  return cached.conn;
 };
 
 export default connectDB;
